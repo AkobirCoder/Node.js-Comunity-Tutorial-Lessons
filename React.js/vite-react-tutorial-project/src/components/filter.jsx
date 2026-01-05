ca91d06e0631180aa0b691cc4df03d5a00e96f83
AkobirCoder <akobir0308@gmail.com>
2026-01-05 23:55:28 +0500

    ./React.js/vite-react-tutorial-project/src/components/filter.jsx was added

---
+++ React.js/vite-react-tutorial-project/src/components/filter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Filter = () => {
+    return (
+        <div className='filter'>
+            <div className='btns'>
+                <button className='active'>All products</button>
+                <button>Completed</button>
+                <button>Big size</button>
+            </div>
+        </div>
+    )
+}
+
+export default Filter
