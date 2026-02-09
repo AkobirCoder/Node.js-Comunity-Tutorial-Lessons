f30280587e0dafc42657fcffc732449e104b818c
AkobirCoder <akobir0308@gmail.com>
2026-02-10 01:28:47 +0500

    ./React.js/vite-react-state-tutorial-app/src/components/card.jsx was updated

--- React.js/vite-react-state-tutorial-app/src/components/card.jsx
+++ React.js/vite-react-state-tutorial-app/src/components/card.jsx
@@ -7,10 +7,20 @@ const Card = ({title, category, image}) => { // Bu yerda props'dan ma'lumotlar t
     // const {title, category, image} = props {/* Bu yerda props'dan ma'lumotlar destrukturizatsiya qilinadi */}
 
     return (
-        <div className='card'>
-            <img style={{borderRadius: 10}} width={'100%'} src={image} alt="Course image" />
-            <h1 style={{marginBottom: 10}}>{title()}</h1>
-            <p style={{margin: 0}}>{category.category}</p>
+        <div className="card">
+            <img
+                style={{ borderRadius: 10 }}
+                width={"100%"}
+                src={image}
+                alt="Course image"
+            />
+            <h1 style={{ marginBottom: 10 }}>{title()}</h1>
+            <p style={{ margin: 0 }}>{category.category} <span>0</span></p>
+            <div className="btn-group mt-3">
+                <button className="btn btn-success">increment</button>
+                <button className="btn btn-danger">decrement</button>
+                <button className="btn btn-primary">reset</button>
+            </div>
         </div>
     )
 }
