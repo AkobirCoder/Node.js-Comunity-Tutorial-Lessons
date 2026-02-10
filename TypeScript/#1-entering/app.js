b09efcc9679d23a518fb3f1c86e1383abd11bd23
AkobirCoder <akobir0308@gmail.com>
2026-02-10 23:57:01 +0500

    ./TypeScript/#1-entering/app.js was added

---
+++ TypeScript/#1-entering/app.js
@@ -0,0 +1,14 @@
+// --- Typescriptda type'larni static qilib yaratadi --- //
+var fullName = 'John Doe';
+// fullName = 123; // bu holatda xatolik chunki fullName yuqorida string type qilib yaratilgan
+fullName = '123456789'; // bu holatda xatolik yo'q, chunki string type'da qiymat berilmoqda
+// --- Quyidagi kodlarda o'zgaruvchi qiymatlarini o'zgartirishda faqatgina daslabki holatda e'lon qilingan type'da qiymat berish kerak --- //
+var text = 'Hello world';
+var isMarried = false;
+var age = 22;
+text = 'Assalomu alaykum';
+isMarried = true;
+age = 40;
+console.log(text);
+console.log(isMarried);
+console.log(age);
