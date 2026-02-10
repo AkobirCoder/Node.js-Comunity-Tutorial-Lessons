90aaecf77619d8f4eed8a240cc0ea936319f8357
AkobirCoder <akobir0308@gmail.com>
2026-02-10 23:38:34 +0500

    ./TypeScript/main-types/app.js was updated

---
+++ TypeScript/main-types/app.js
@@ -0,0 +1,8 @@
+// Asosiy type'lar: string, number, boolean
+var fullName = 'John Doe';
+// console.log(typeof(fullName));
+console.log(typeof fullName); // type -> string
+var salary = 10000;
+var bonus = 5000;
+var totalSalary = salary + bonus;
+console.log(totalSalary);
