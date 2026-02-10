631ad7311b200bd391601c0b77ea0d9f7479eec9
AkobirCoder <akobir0308@gmail.com>
2026-02-10 20:14:06 +0500

    ./TypeScript/app.ts was added

---
+++ TypeScript/app.ts
@@ -0,0 +1,21 @@
+// --- Typescriptda type'larni static qilib yaratadi --- //
+
+let fullName = 'John Doe';
+
+// fullName = 123; // bu holatda xatolik chunki fullName yuqorida string type qilib yaratilgan
+
+fullName = '123456789'; // bu holatda xatolik yo'q, chunki string type'da qiymat berilmoqda
+
+// --- Quyidagi kodlarda o'zgaruvchi qiymatlarini o'zgartirishda faqatgina daslabki holatda e'lon qilingan type'da qiymat berish kerak --- //
+
+let text: string = 'Hello world';
+let isMarried: boolean = false;
+let age: number = 22;
+
+text = 'Assalomu alaykum';
+isMarried = true;
+age = 45;
+
+console.log(text);
+console.log(isMarried);
+console.log(age);
