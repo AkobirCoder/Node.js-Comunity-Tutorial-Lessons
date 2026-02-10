47c283fa6eae12e0560245104d8ac1612475857d
AkobirCoder <akobir0308@gmail.com>
2026-02-10 23:51:53 +0500

    ./TypeScript/main-types/app.js was updated

--- TypeScript/main-types/app.js
+++ TypeScript/main-types/app.js
@@ -6,3 +6,7 @@ var salary = 10000;
 var bonus = 5000;
 var totalSalary = salary + bonus;
 console.log(totalSalary);
+var salary1 = 20000;
+var bonus1 = '10000';
+var totalSalary1 = salary1 + bonus1;
+console.log(totalSalary1); // 2000010000 (string concatenation)
