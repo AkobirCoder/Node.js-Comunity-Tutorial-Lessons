a309cce52073b25e0b28fddf182c3ae8700d9f9d
AkobirCoder <akobir0308@gmail.com>
2026-02-15 00:26:00 +0500

    ./TypeScript/#4-object/app.ts was updated

---
+++ TypeScript/#4-object/app.ts
@@ -0,0 +1,24 @@
+const person = {
+    firstName: 'Akobir', // required
+    lastName: 'Usmonov', // required
+    city: 'Tashkent',
+    age: 22,
+    skills: {
+        programming: ['JavaScript', 'TypeScript', 'Python'],
+        design: ['Photoshop', 'Illustrator', 'Figma'],
+    }
+}
+
+function greetingPerson(data: {firstName: string, lastName: string}): string {
+    return `${data.firstName} ${data.lastName}`;
+}
+
+// --- Agar TypeScriptda `person` obyektida `firstName` xususiyati mavjud bo'masa, `greetingPerson` funksiyasini chaqirishda xato yuz beradi --- //
+// --- Chunki firstName va lastName xususiyatlari `greetingPerson` funksiyasining parametrlarida required (majburiy) sifatida belgilangan --- //
+// --- Shuning uchun ham TypeScript JavaScriptga nisbatan ko'proq xavfsizlikni ta'minlaydi, chunki u kodni kompilyatsiya qilishdan oldin xatolarni aniqlaydi --- //
+
+const greeting = greetingPerson(person);
+
+console.log(greeting); // Output: "Akobir Usmonov"
+
+console.log('------------------------------');
