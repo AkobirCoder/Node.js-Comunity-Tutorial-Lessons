63a4f38cf0f2a421b7b700e7a797f80e49815cf1
AkobirCoder <akobir0308@gmail.com>
2026-02-14 22:08:45 +0500

    ./React.js/vite-react-tutorial-project/src/components/shopping-add-form.jsx was updated

---
+++ React.js/vite-react-tutorial-project/src/components/shopping-add-form.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+class ShoppingAddForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: '',
+            number: '',
+        }
+    }
+
+    onChangeHandler = (event) => {
+        const {name, value} = event.target;
+
+        this.setState((prevState) => {
+            return {...prevState, [name]: value}
+        })
+    }
+
+    onAdd = (event) => {
+        event.preventDefault()
+
+        const {title, number} = this.state
+
+        const data = {
+            title: title,
+            number: number,
+        }
+
+        this.setState(() => {
+            return {
+                title: '',
+                number: '',
+            }
+        })
+
+        console.log(data)
+    }
+
+    render() {
+        const {title, number} = this.state
+
+        const {onChangeHandler, onAdd} = this
+
+        return (
+            <form className='form' onSubmit={onAdd}>
+                <input 
+                    type="text" 
+                    name={'title'}
+                    value={title}
+                    className='input-title' 
+                    placeholder='Title...' 
+                    onChange={onChangeHandler} 
+                />
+                <input 
+                    type="number" 
+                    name={'number'} 
+                    value={number}
+                    className='input-number' 
+                    placeholder='14' 
+                    onChange={onChangeHandler} 
+                />
+                <button type='submit'>Add</button>
+            </form>
+        )
+    }
+}
+
+export default ShoppingAddForm
