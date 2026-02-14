d55641804675d89c63d0a5b96a8c0554ba67ad47
AkobirCoder <akobir0308@gmail.com>
2026-02-14 23:16:44 +0500

    ./TypeScript/#2-main-types/app.ts was updated

--- TypeScript/#2-main-types/app.ts
+++ TypeScript/#2-main-types/app.ts
@@ -6,12 +6,17 @@ let fullName: string = 'John Doe';
 
 console.log(typeof fullName); // type -> string
 
+console.log('-------------------');
+
+
 const salary: number = 10000;
 const bonus: number = 5000;
 
 const totalSalary = salary + bonus; // 15000 (number)
 
 console.log(totalSalary);
+
+console.log('-------------------');
 
 
 // --- String va number'ni qo'shish --- //
@@ -24,6 +29,8 @@ const bonus1 = '10000';
 const totalSalary1 = salary1 + bonus1;
 
 console.log(totalSalary1); // 2000010000 (string concatenation)
+
+console.log('-------------------');
 
 
 // --- Bu yerda xatolik yuz beradi, chunki isEmployed o'zgaruvchisi boolean turida e'lon qilingan va unga string qiymat berilmoqda. --- //
