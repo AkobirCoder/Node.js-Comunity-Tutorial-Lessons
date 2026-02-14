879c16c732fd012507b18433d4520183e84c1c2f
AkobirCoder <akobir0308@gmail.com>
2026-02-14 22:08:11 +0500

    ./React.js/vite-react-tutorial-project/src/components/app.jsx was updated;

--- React.js/vite-react-tutorial-project/src/components/app.jsx
+++ React.js/vite-react-tutorial-project/src/components/app.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import Info from './shopping-info'
-import ShoppingForm from './shopping-form'
+import ShoppingAddForm from './shopping-add-form'
 import ShoppingList from './shopping-list'
 import Filter from './filter'
 
 const App = () => {
+    const data = [
+        {
+            id: 1,
+            size: 14,
+            title: 'Buy Bananas',
+            active: false,
+        },
+        {
+            id: 2,
+            size: 10,
+            title: 'Buy Ananas',
+            active: true,
+        },
+        {
+            id: 3,
+            size: 8,
+            title: 'Buy Milk',
+            active: false,
+        },
+    ]
+
     return (
         <div className='app'>
             <div className='wrapper'>
                 <div className='wrapper-card'>
                     <Info />
-                    <ShoppingForm />
-                    <ShoppingList />
+                    <ShoppingAddForm />
+                    <ShoppingList data={data} />
                     <Filter />
                 </div>
                 <img src="/earth.svg" alt="Earth image" />
