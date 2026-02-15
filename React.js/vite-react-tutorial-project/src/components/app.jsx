f09a8ad0bcafcb35ef2cb87290757305ef562629
AkobirCoder <akobir0308@gmail.com>
2026-02-16 01:34:04 +0500

    ./React.js/vite-react-tutorial-project/src/components/app.jsx was updated

--- React.js/vite-react-tutorial-project/src/components/app.jsx
+++ React.js/vite-react-tutorial-project/src/components/app.jsx
@@ -1,25 +1,88 @@
 import React, { Component } from 'react'
+import { v4 as uuid4 } from 'uuid'
 import { Filter, ShoppingAddForm, ShoppingInfo, ShoppingList } from './'
-import { data } from '../constants/'
+import { array } from '../constants/'
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: data
+            data: array,
         }
+    }
+
+    onDelete = (id) => {
+        const {data} = this.state
+
+        const newArray = data.filter((dataItem) => {
+            return dataItem.id !== id
+        })
+
+        this.setState(() => {
+            return {
+                data: newArray
+            }
+        })
+
+        // console.log(newArray)
+    }
+
+    onToggleActive = (id) => {
+        const {data} = this.state
+
+        const newArray = data.map((dataItem) => {
+            if (dataItem.id === id) {
+                return {...dataItem, active: !dataItem.active}
+            } else {
+                return dataItem
+            }
+        })
+
+        this.setState(() => {
+            return {
+                data: newArray
+            }
+        })
+
+        // console.log(newArray)
+    }
+
+    onAdd = (item) => {
+        const {data} = this.state
+
+        const {title, number} = item
+
+        const newData = {title, size: number, active: false, id: uuid4()}
+
+        const newArray = [...data, newData]
+
+        this.setState(() => {
+            return {
+                data: newArray
+            }
+        })
+
+        // console.log(newArray)
     }
 
     render() {
         const {data} = this.state
 
+        const {onDelete, onToggleActive, onAdd} = this
+
+        const dataLength = data.length
+
         return (
             <div className='app'>
                 <div className='wrapper'>
                     <div className='wrapper-card'>
-                        <ShoppingInfo />
-                        <ShoppingAddForm />
-                        <ShoppingList data={data} />
+                        <ShoppingInfo dataLength={dataLength} />
+                        <ShoppingAddForm onAdd={onAdd} />
+                        <ShoppingList 
+                            data={data} 
+                            onDelete={onDelete} 
+                            onToggleActive={onToggleActive}
+                        />
                         <Filter />
                     </div>
                     <img src="/earth.svg" alt="Earth image" />
