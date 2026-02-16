3374754d83ac7189f6670c28689a90e977669247
AkobirCoder <akobir0308@gmail.com>
2026-02-16 17:57:52 +0500

    ./React.js/vite-react-tutorial-project/src/components/filter.jsx was updated

--- React.js/vite-react-tutorial-project/src/components/filter.jsx
+++ React.js/vite-react-tutorial-project/src/components/filter.jsx
@@ -6,12 +6,36 @@ class Filter extends Component {
     }
 
     render() {
+        const buttons = [
+            {
+                id: 'all', label: 'All products',
+            },
+            {
+                id: 'completed', label: 'Completed',
+            },
+            {
+                id: 'big-size', label: 'Big size',
+            },
+        ]
+
+        const {filter, onFilterSelect} = this.props
+
         return (
             <div className='filter'>
                 <div className='btns'>
-                    <button className='active'>All products</button>
-                    <button>Completed</button>
-                    <button>Big size</button>
+                    {
+                        buttons.map((btn) => {
+                            return (
+                                <button 
+                                    key={btn.id} 
+                                    className={`${filter === btn.id && 'active'}`}
+                                    onClick={() => onFilterSelect(btn.id)}
+                                >
+                                    {btn.label}
+                                </button>
+                            )
+                        })
+                    }
                 </div>
             </div>
         )
