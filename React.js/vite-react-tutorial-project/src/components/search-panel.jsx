46b5a3ff526aaa22c980e7dd5a96472e25405fab
AkobirCoder <akobir0308@gmail.com>
2026-02-16 16:54:39 +0500

    ./React.js/vite-react-tutorial-project/src/components/search-panel.jsx was updated

--- React.js/vite-react-tutorial-project/src/components/search-panel.jsx
+++ React.js/vite-react-tutorial-project/src/components/search-panel.jsx
@@ -3,14 +3,38 @@ import React, { Component } from 'react'
 class SearchPanel extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            search: '',
+        }
+    }
+
+    onChangeHandler = (event) => {
+        const {name, value} = event.target
+
+        const {onUpdateSearch} = this.props
+
+        this.setState((prevState) => {
+            return {
+                ...prevState, [name]: value,
+            }
+        })
+
+        onUpdateSearch(value)
     }
     
     render() {
+        const {search} = this.state
+
+        const {onChangeHandler} = this
+
         return (
             <input 
+                type="text"
                 className='search' 
-                placeholder='Search...' 
-                type="text" 
+                placeholder='Search...'  
+                name={'search'}
+                value={search}
+                onChange={onChangeHandler}
             />
         )
     }   
