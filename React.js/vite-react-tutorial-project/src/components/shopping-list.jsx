6e53b5f75cff5639858567def9858b02a1083dfd
AkobirCoder <akobir0308@gmail.com>
2026-02-16 16:29:17 +0500

    ./React.js/vite-react-tutorial-project/src/components/shopping-list.jsx was updated

--- React.js/vite-react-tutorial-project/src/components/shopping-list.jsx
+++ React.js/vite-react-tutorial-project/src/components/shopping-list.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { ShoppingListItem } from './'
 
-const ShoppingList = ({data, onDelete, onToggleActive}) => {
-    return (
-        <div className='shopping-list'>
-            {
-                data.map((item) => {
-                    return (
-                        <ShoppingListItem
-                            key={item.id}
-                            item={item}
-                            onDeleteItem={() => onDelete(item.id)}
-                            onToggleActiveItem={() => onToggleActive(item.id)}
-                        />
-                    )
-                })
-            }
-        </div>
-    )
+class ShoppingList extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {data, onDelete, onToggleActive} = this.props
+
+        return (
+            <div className='shopping-list'>
+                {
+                    data.map((item) => {
+                        return (
+                            <ShoppingListItem
+                                key={item.id}
+                                item={item}
+                                onDeleteItem={() => onDelete(item.id)}
+                                onToggleActiveItem={() => onToggleActive(item.id)}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+    
 }
 
 export default ShoppingList
